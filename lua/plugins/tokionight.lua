cdaf03a5e0d9e0a45650d71a7d6865b5f6783d30
MJ Adendorff <mj@quicklysign.com>
2024-08-25 22:27:02 +0200

    changed colorscheme

--- lua/plugins/tokionight.lua
+++ lua/plugins/tokionight.lua
@@ -1,9 +1,10 @@
 return {
 	"folke/tokyonight.nvim",
 	lazy = false,
+	enabled = false,
 	priority = 1000,
-    config = function ()
-        require("tokyonight").setup()
-        vim.cmd("colorscheme tokyonight-night")
-    end
+	config = function()
+		require("tokyonight").setup()
+		vim.cmd("colorscheme tokyonight-night")
+	end,
 }
---
+++ lua/plugins/tokyodark.lua
@@ -0,0 +1,9 @@
+return {
+	"tiagovla/tokyodark.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("tokyodark").setup()
+		vim.cmd("colorscheme tokyodark")
+	end,
+}
